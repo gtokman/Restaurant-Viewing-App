2e9c4b6d49d7d60eacb678c87def04c6592a38e9
Gary Guerman Tokman <gtokman@fullsail.edu>
2018-02-25 20:53:35 -0500

    Initial Commit

---
+++ RestaurantApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RestaurantApp
+//
+//  Created by Gary Tokman on 2/25/18.
+//  Copyright © 2018 Gary Tokman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
