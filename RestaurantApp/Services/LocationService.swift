7ec8f2a6bbab123482528ba0bcf0e0704de05409
Gary Tokman <gtokman@fullsail.edu>
2018-11-17 23:58:00 -0500

    Add location service

---
+++ RestaurantApp/Services/LocationService.swift
@@ -0,0 +1,61 @@
+//
+//  LocationService.swift
+//  RestaurantApp
+//
+//  Created by Gary Tokman on 11/17/18.
+//  Copyright © 2018 Gary Tokman. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+enum Result<T> {
+    case success(T)
+    case failure(Error)
+}
+
+final class LocationService: NSObject {
+    private let manager: CLLocationManager
+
+    init(manager: CLLocationManager = .init()) {
+        self.manager = manager
+        super.init()
+        manager.delegate = self
+    }
+
+    var newLocation: ((Result<CLLocation>) -> Void)?
+    var didChangeStatus: ((Bool) -> Void)?
+
+    var status: CLAuthorizationStatus {
+        return CLLocationManager.authorizationStatus()
+    }
+
+    func requestLocation() {
+        manager.requestWhenInUseAuthorization()
+    }
+
+    func getLocation() {
+        manager.requestLocation()
+    }
+}
+
+extension LocationService: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        newLocation?(.failure(error))
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.sorted(by: {$0.timestamp > $1.timestamp}).first {
+            newLocation?(.success(location))
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined, .restricted, .denied:
+            didChangeStatus?(false)
+        default:
+            didChangeStatus?(true)
+        }
+    }
+}
