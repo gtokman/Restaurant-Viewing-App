3dd964d311fe3837e6b99e26c282b664ded232e9
Gary Tokman <gtokman@fullsail.edu>
2018-11-21 01:01:01 -0500

    Install cocoapods

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '11.0'
+use_frameworks!
+
+target 'RestaurantApp' do
+    pod 'AlamofireImage'
+    pod 'Moya'
+end
---
+++ Pods/Target Support Files/Pods-RestaurantApp/Pods-RestaurantApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RestaurantApp : NSObject
+@end
+@implementation PodsDummy_Pods_RestaurantApp
+@end
